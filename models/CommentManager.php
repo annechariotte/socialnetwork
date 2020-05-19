f7afd4fa7b65c7bd0f767ad583992cf7b9874548
Anne-Charlotte <ac.drevensek.deldicque@gmail.com>
2020-05-19 12:44:05 +0200

    first commit

--- models/CommentManager.php
+++ models/CommentManager.php
@@ -20,9 +20,21 @@ function GetAllCommentsFromUserId($userId)
   global $PDO;
   $response = $PDO->query(
     "SELECT comment.*, user.nickname "
-      . "FROM comment LEFT JOIN user on (comment.user_id = user.id) "
-      . "WHERE comment.user_id = $userId "
-      . "ORDER BY comment.created_at ASC"
+      . " FROM comment LEFT JOIN user on (comment.user_id = user.id) "
+      . " WHERE comment.user_id = $userId "
+      . " ORDER BY comment.created_at ASC "
+  );
+  return $response->fetchAll();
+}
+
+function GetAllCommentsFromPostId($postId)
+{
+  global $PDO;
+  $response = $PDO->query(
+    " SELECT comment.*, user.nickname "
+      . " FROM comment LEFT JOIN user on (comment.user_id = user.id) "
+      . " WHERE comment.post_id = $postId "
+      . " ORDER BY comment.created_at ASC "
   );
   return $response->fetchAll();
 }
